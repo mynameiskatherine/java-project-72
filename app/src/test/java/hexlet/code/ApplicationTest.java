255e1f16e151059d49c3c9810a34f0509b0a2dce
Kate <e.sheverdenok@gmail.com>
2024-02-23 10:54:57 +0000

    project configuration

---
+++ app/src/test/java/hexlet/code/ApplicationTest.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ApplicationTest {
+
+    @Test
+    void testStringRequiredOption() {
+        var actual = "";
+        var expected = "";
+        assertThat(actual).isEqualTo(expected);
+    }
+}
