02f0a66ac1fe09b4b2dc5fbc877bcbd14869f4bb
Kate <e.sheverdenok@gmail.com>
2024-03-27 10:02:32 +0000

    adding postgre as prod db

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ repositories {
 
 dependencies {
     implementation("com.h2database:h2:2.2.224")
+    implementation("org.postgresql:postgresql:42.7.3")
     implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     implementation("org.apache.commons:commons-text:1.11.0")
