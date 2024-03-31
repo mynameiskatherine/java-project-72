05138d043c59bf27b21bfc2db2673930171468b4
Kate <e.sheverdenok@gmail.com>
2024-03-31 18:23:39 +0000

    fixing tests

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -42,27 +42,30 @@ public final class App {
     public static void setDataSource() throws IOException {
         HikariConfig hikariConfig = new HikariConfig();
         HikariDataSource dataSource;
+        String sql;
         String dbUrl = System.getenv("JDBC_DATABASE_URL");
         if (dbUrl != null) {
             hikariConfig.setJdbcUrl(dbUrl);
-            hikariConfig.setUsername(System.getenv().get("JDBC_DB_USERNAME"));
-            hikariConfig.setPassword(System.getenv().get("JDBC_DB_PASSWORD"));
-            hikariConfig.setDriverClassName(org.postgresql.Driver.class.getName());
-            dataSource = new HikariDataSource(hikariConfig);
+//            hikariConfig.setUsername(System.getenv().get("JDBC_DB_USERNAME"));
+//            hikariConfig.setPassword(System.getenv().get("JDBC_DB_PASSWORD"));
+//            hikariConfig.setDriverClassName(org.postgresql.Driver.class.getName());
+            sql = Utils.readFileFromResources("schemaPostgres.sql");
         } else {
             hikariConfig.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
-            dataSource = new HikariDataSource(hikariConfig);
-            var sql = Utils.readFileFromResources("schema.sql");
-
-            log.info(sql);
-
-            try (Connection connection = dataSource.getConnection();
-                 Statement statement = connection.createStatement()) {
-                statement.execute(sql);
-            } catch (Exception e) {
-                throw new RuntimeException();
-            }
+            sql = Utils.readFileFromResources("schema.sql");
         }
+
+        dataSource = new HikariDataSource(hikariConfig);
+
+        log.info(sql);
+
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+
         BaseRepository.dataSource = dataSource;
     }
 
