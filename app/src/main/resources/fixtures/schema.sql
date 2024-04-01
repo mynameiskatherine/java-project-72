7a0cc8d294a64d89d2649cae0659383a814b4e1b
Kate <e.sheverdenok@gmail.com>
2024-04-01 08:41:43 +0000

    fixing tests

--- app/src/main/resources/fixtures/schema.sql
+++ app/src/main/resources/fixtures/schema.sql
@@ -10,7 +10,8 @@ CREATE TABLE urls (
 CREATE TABLE url_checks (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
     CONSTRAINT pk_url_checks PRIMARY KEY (id),
-    url_id BIGINT REFERENCES urls(id),
+    url_id BIGINT,
+    CONSTRAINT fk_url_id FOREIGN KEY(url_id) REFERENCES urls(id),
     status_code INT,
     h1 VARCHAR(255),
     title VARCHAR(255),
