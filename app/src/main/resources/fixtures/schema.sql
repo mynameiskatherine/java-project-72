319bc97fb5cf441a1ab1bcb309aaf54544697fc4
Kate <e.sheverdenok@gmail.com>
2024-04-01 07:38:35 +0000

    fixing tests

--- app/src/main/resources/fixtures/schema.sql
+++ app/src/main/resources/fixtures/schema.sql
@@ -2,12 +2,14 @@ DROP TABLE IF EXISTS urls, url_checks;
 
 CREATE TABLE urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    CONSTRAINT pk_urls PRIMARY KEY (id),
     name VARCHAR(255),
     created_at TIMESTAMP
 );
 
 CREATE TABLE url_checks (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    CONSTRAINT pk_url_checks PRIMARY KEY (id),
     url_id BIGINT REFERENCES urls(id),
     status_code INT,
     h1 VARCHAR(255),
