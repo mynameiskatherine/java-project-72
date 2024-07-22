e4abad99a12d2948bcf9d1c169b3c41ab42d1f2f
Kate <125584522+mynameiskatherine@users.noreply.github.com>
2024-07-22 14:43:32 +0300

    Update Lombok dependency version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     checkstyle
     jacoco
     application
-    id("io.freefair.lombok") version "8.3"
+    id("io.freefair.lombok") version "8.6"
     id("com.github.ben-manes.versions") version "0.47.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
